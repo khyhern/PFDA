3addeda05fc8572c398351ad70290aaebb591ae7
StarryStuff <starrystufff@gmail.com>
2025-02-05 21:31:05 +0800

    Cleaned Downtime, Ransom, Loss

--- Azwa_PFDA.R
+++ Azwa_PFDA.R
@@ -26,3 +26,33 @@ hackingdata <- distinct(hackingdata)
 nrow(hackingdata) #[211913]
 
 
+#Missing Values
+
+hackingdata %>%
+  summarise(across(everything(), ~ sum(is.na(.))))
+
+mcar_test(hackingdata) # p-value < 0.5 thus must impute
+
+#DownTime [0 Missing Values]
+
+#Ransom [159,820 Missing Values] 
+#Since DownTime is a factor of Loss - And DownTime is never missing then there will always be loss
+#But it does not mean the Ransom is always a factor as it could be 0
+hackingdata <- hackingdata %>%
+  mutate(Ransom = ifelse(is.na(Ransom), 0, Ransom))
+
+#Loss [33,594 Missing Values] 
+#Loss Mean = 2771.061
+hackingdata$Loss[is.na(hackingdata$Loss)] <- mean(hackingdata$Loss, na.rm = TRUE)
+
+
+#Outliers in Loss
+Loss_Q1 <- quantile(hackingdata$Loss, 0.25)
+Loss_Q3 <- quantile(hackingdata$Loss, 0.75)
+Loss_IQR <- Loss_Q3 - Loss_Q1
+Loss_LB <- Loss_Q1 - 1.5*Loss_IQR
+Loss_UB <- Loss_Q3 + 1.5*Loss_IQR
+Loss_Outliers <- hackingdata$Loss[hackingdata$Loss < Loss_LB | hackingdata$Loss > Loss_UB]
+
+hackingdata$Loss <- ifelse(hackingdata$Loss > Loss_UB, Loss_UB, hackingdata$Loss)
+hackingdata$Loss <- ifelse(hackingdata$Loss < Loss_LB, Loss_LB, hackingdata$Loss)
